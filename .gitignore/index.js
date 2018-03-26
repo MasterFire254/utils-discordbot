c07e64c2730beb8b15b555cc27f9a3e053f1c7e4
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-26 18:35:43 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,24 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
 
-var prefix = (".");
+var prefix = (".")
 var secondaryPrefix = ("?")
-
+var game = (".help")
 
 
 bot.on('ready', function(){
     
     console.log("Connected");
-    bot.user.setGame(".help");
+    bot.user.setGame(game);
 });
 
 bot.on('message', message =>{
 
-    if(message.content === secondaryPrefix + "bg"){
+    if(message.content === "?bg"){
         message.channel.sendMessage("C'est Benedict ");
     };
 
-    if(message.content === prefix + "help"){
+    if(message.content === ".help"){
 
         //commande help
         var embed = new discord.RichEmbed()
@@ -29,40 +29,39 @@ bot.on('message', message =>{
             .addField(".helpmusic","Permet de voir les commandes pour les musics")
             message.channel.sendEmbed(embed);
 
-        //commande kick
+    };
+    //commande kick    
+    if(message.content === ".kick"){
+
         let command = message.content.slipt(" ")[0];
         const args = message.content.slice(prefix.length).split(/ +/);
         command = args.shift().toLowerCase();
-
-
-    }
         
-    if(command === prefix + "kick"){
             let modRole = message.guild.roles.find("name", "Fondateur");
             if(!message.member.roles.has(modRole.id)){
-                return message.reply("Tu n'as pas la permission pour cette commande.").catch(console.error);
-            }
+                return message.reply("Tu n'as pas la permission pour cette commande.");
+            };
 
             if(message.mentions.users.size === 0){
-                return message.reply("Merci de mentionner l'utilisateur è kicker.").catch(console.error);
-            }
+                return message.reply("Merci de mentionner l'utilisateur è kicker.");
+            };
 
             let kickMember = message.guild.member(message.mentions.users.first());
             if(!kickMember){
                 return message.reply("Cet utilisateur est introuvable ou impossible à kicker.");
-            }
+            };
             
             if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
-                return message.reply("Je n'ai pas la permission de kick.")
-            }
+                return message.reply("Je n'ai pas la permission de kick.");
+            };
 
         kickMember.kick().then(member =>{
-        message.reply(`${member.user.username} a bien été kick.`).catch(console.error);
+        message.reply(`${member.user.username} a bien été kick.`);
         message.guild.channels.find("name", "general").send(`**${member.user.username} a été kick par **${message.author.username}**.`);
-        }).catch(console.error)
+        });
         
 
-    }
+    };
 });
 
 
