37beaf2d9be85984f6e13b82ae32a91d37148ff1
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-26 17:27:18 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,10 +1,10 @@
-const Discord = require('discord.js');
+sconst Discord = require('discord.js');
 const bot = new Discord.Client();
 
 var prefix = (".");
 var secondaryPrefix = ("?")
 
-bot.login("MzY5OTI5Mzk2ODA4NTgxMTIw.DZliNA.aerFeGAHsctRyrsjksSwT5ng5h8");
+bot.login(process.enc.TOKEN);
 
 bot.on('ready', function(){
     
