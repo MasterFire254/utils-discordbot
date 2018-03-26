242c8e8b411ab2afd4b427c78f498d905bb28976
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-26 17:51:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,15 @@
-sconst Discord = require('discord.js');
+const d = require('discord.js');
 const bot = new Discord.Client();
 
 var prefix = (".");
 var secondaryPrefix = ("?")
 
-bot.login(process.enc.TOKEN);
+
 
 bot.on('ready', function(){
     
     console.log("Connected");
+    bot.user.setGame(".help");
 });
 
 bot.on('message', message =>{
@@ -17,6 +18,16 @@ bot.on('message', message =>{
         message.channel.sendMessage("C'est Benedict ");
     };
 
+    if(message.content === prefix + "help"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("Page d'aide")
+            .addField(".ban [pseudo] ","Permet de ban des joueurs")
+            .addBlankField()
+            .addField(".kick [pseudo]", "Permet de kick des joueurs")
+            .addBlankField()
+            message.channel.sendEmbed(embed);
+    }
 
 });
 
+bot.login(process.env.TOKEN);
