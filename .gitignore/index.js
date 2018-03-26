2e799af66ab57f374497c9b20702e82cace8c9ea
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-26 19:15:38 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,19 +8,17 @@ var game = (".help")
 
 bot.on('ready', function(){
     
-    console.log(`Connecté avec ${bot.user.tag} (${bot.user.id}) sur ${bot.guilds.size} `);
+    console.log(`Connecté avec ${bot.user.tag} (${bot.user.id}) sur ${bot.guilds.size} serveurs`);
     bot.user.setGame(game);
 });
 
-bot.on('message', message =>{
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+bot.on('message', msg =>{;
 
-    if(message.content === secondaryPrefix + "bg"){
-        message.channel.sendMessage("C'est Benedict ");
+    if(msg.content === secondaryPrefix + "bg"){
+        msg.channel.sendmsg("C'est Benedict ");
     };
 
-    if(command === "help"){
+    if(msg.content === "help"){
 
         //commande help
         var embed = new discord.RichEmbed()
@@ -29,44 +27,35 @@ bot.on('message', message =>{
             .addField(".kick [@pseudo]", "Permet de kick des joueurs")
             .addField(".changecolor [@role]","Permet de changer la couleur d'un role")
             .addField(".helpmusic","Permet de voir les commandes pour les musics")
-            message.channel.sendEmbed(embed);
+            msg.channel.sendEmbed(embed);
 
     };
 
     //commande kick    
-    if(command === "kick"){
+    if(msg.content === "kick"){
 
-            const member = message.mentions.members.first();
+            const member = msg.mentions.members.first();
 
-            if(!message.member.permissions.has('KICK_MEMBERS')){
-                return message.reply("Tu n'as pas la permission pour cette commande.");
+            if(!msg.member.permissions.has('KICK_MEMBERS')){
+                return msg.reply("Tu n'as pas la permission pour cette commande.");
             };
 
-            if(message.mentions.users.size === 0){
-                return message.reply("Merci de mentionner l'utilisateur è kicker.");
+            if(msg.mentions.users.size === 0){
+                return msg.reply("Merci de mentionner l'utilisateur è kicker.");
             };
 
-            let kickMember = message.guild.member(message.mentions.users.first());
-            if(!kickMember){
-                return message.reply("Cet utilisateur est introuvable ou impossible à kicker.");
+            if(!member){
+                return msg.reply("Cet utilisateur est introuvable ou impossible à kicker.");
             };
             
-            if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")){
-                return message.reply("Je n'ai pas la permission de kick.");
-            };
-
-        kickMember.kick().then(member =>{
-        message.reply(`${member.user.username} a bien été kick.`);
-        message.guild.channels.find("name", "general").send(`**${member.user.username} a été kick par **${message.author.username}**.`);
-        });
         
 
     };
 
-    if(message.content === prefix + "ping"){
+    if(msg.content === prefix + "ping"){
         const then = Date.now();
-        message.channel.send('Pinging...').then(m =>{
-            m.edit(`Pong! Ca a pris  ${Date.now() - then}ms pour envoyer ce message\nDiscord bot : ${bot.ping}ms`);
+        msg.channel.send('Pinging...').then(m =>{
+            m.edit(`Pong! Ca a pris  ${Date.now() - then}ms pour envoyer ce msg\nDiscord bot : ${bot.ping}ms`);
         });
     }
 
