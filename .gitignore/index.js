98c423e8a47c28230cd1665e7d9322efbca916a8
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-26 17:18:47 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = (".");
+var secondaryPrefix = ("?")
+
+bot.login("MzY5OTI5Mzk2ODA4NTgxMTIw.DZliNA.aerFeGAHsctRyrsjksSwT5ng5h8");
+
+bot.on('ready', function(){
+    
+    console.log("Connected");
+});
+
+bot.on('message', function(message){
+
+
+    if(message.content === secondaryPrefix + "bg"){
+        message.channel.sendMessage("C'est Benedict ");
+    };
+
+
+});
+
