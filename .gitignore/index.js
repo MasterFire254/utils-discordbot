1145523e0035eec15430d565d44a9eb8d04c2e18
MasterFire254 <35341547+MasterFire254@users.noreply.github.com>
2018-03-27 12:06:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,24 +1,45 @@
 const discord = require('discord.js');
+const YTDL = require('ytdl-core');
 const bot = new discord.Client();
 
 var prefix = (".")
 var secondaryPrefix = ("?")
 
+function play(connection, message){
+    var server = servers[message.guild.id];
+
+    server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
+
+    server.queue.shift();
+
+    server.dispatcher.on("end", function(){
+        if(server.queue[0]){
+            play(connection, message);
+        }else{
+            connection.disconect();
+        }
+    });
+}
+
+var servers = {};
 
 bot.on('ready', function(){
     
     console.log(`Connecté avec ${bot.user.tag} (${bot.user.id}) sur ${bot.guilds.size} serveurs`);
+    bot.user.setGame('.help');
 });
 
-bot.on('message', msg =>{;
+bot.on('message', message =>{;
 
-    if(msg.content === secondaryPrefix + "bg"){
-        msg.channel.sendMessage("C'est Benedict ");
+    var args = message.content.substring(prefix.length).split(' ');
+
+    if(message.content === secondaryPrefix + "bg"){
+        message.channel.sendMessage("C'est Benedict ");
     };
 
-    if(msg.content === prefix + "help"){
+    if(message.content === prefix + "help"){
 
-        //commande help
+        //HELP
         var embed = new discord.RichEmbed()
             .setTitle("Page d'aide")
             .addField(".ban [@pseudo] ","Permet de ban des joueurs")
@@ -26,51 +47,86 @@ bot.on('message', msg =>{;
             .addField(".changecolor [@role]","Permet de changer la couleur d'un role")
             .addField(".helpmusic","Permet de voir les commandes pour les musics")
             .setColor(255, 0, 0)
-            msg.channel.sendEmbed(embed);
+            message.channel.sendEmbed(embed);
 
     };
 
     //PING
-    if(msg.content === prefix + "ping"){
+    if(message.content === prefix + "ping"){
         const then = Date.now();
-        msg.channel.send('Pinging...').then(m =>{
-            m.edit(`Pong! Ca a pris  ${Date.now() - then}ms pour envoyer ce msg\nPing du bot : ${bot.ping}ms`);
+        message.channel.send('Pinging...').then(m =>{
+            m.edit(`Pong! Ca a pris  ${Date.now() - then}ms pour envoyer ce message\nPing du bot : ${bot.ping}ms`);
         });
     }
     //KICK
-    if(msg.content.startsWith(prefix + "kick")){
-        if(!msg.member.permissions.has('KICK_MEMBERS')) return msg.reply("Tu n'as pas les permissions nécéssaires.");
-        const member = msg.mentions.members.first();
-        if(!member) return msg.reply("Mauvais usage fait comme ça : `.kick @User#1234`");
-        member.kick(`Kicker par ${msg.author.username}`);
-        
+    if(message.content.startsWith(prefix + "kick")){
+        if(!message.member.permissions.has('KICK_MEMBERS')) return message.reply("Tu n'as pas les permissions nécéssaires.");
+        const member = message.mentions.members.first();
+        if(!member) return message.reply("Mauvais usage fait comme ça : `.kick @User#1234`");
+        if(member && message.member.permissions.has("KICK_MEMBERS")){
+        member.kick(`Kicker par ${message.author.tag}`);
+        message.reply("Le joueur a bien été kiker.");
+        }
 
     }
 
     //BAN
-    if(msg.content.startsWith(prefix + "ban")){
-        if(!msg.member.permissions.has("BAN_MEMBERS")) return msg.reply("Tu n'as pas les permissions nécéssaires.");
-        const member = msg.mentions.members.first();
-        if(!member) return msg.reply("Mauvais usage fait comme ça : `.ban @User#1234`");
-         if(member && msg.member.permissions.has("BAN_MEMBERS")){
-             member.ban({
-                 reason: `banni par ${msg.author.tag}`
-             });
+    if(message.content.startsWith(prefix + "ban")){
+        if(!message.member.permissions.has("BAN_MEMBERS")) return message.reply("Tu n'as pas les permissions nécéssaires.");
+        const member = message.mentions.members.first();
+        if(!member) return message.reply("Mauvais usage fait comme ça : `.ban @User#1234`");
+         if(member && message.member.permissions.has("BAN_MEMBERS")){
+             member.ban(`banni par ${message.author.tag}`);
+             message.reply("Le joueur a bien été banni.");
          }
 
     }
 
     //MUSIC
-    if(msg.content === prefix + "helpmusic"){
+    if(message.content === prefix + "helpmusic"){
         var embed = new discord.RichEmbed()
             .setTitle("Help Music")
             .addField(".play [link]", "Permet de jouer une music a partir d'un lien youtube.")
             .addField(".stop", "Permet de stopper la music.")
             .addField(".skip","Permet de passer à la music suivante.")
             .setColor(0, 255, 0)
-            msg.channel.sendEmbed(embed);
+            message.channel.sendEmbed(embed);
     }
 
+    if(message.content.startsWith(prefix + "play")){
+        if(!args[1]){
+            message.reply("Met un lien putain");
+            return;
+        }
+
+        if(!message.member.voiceChannel){
+            message.reply("Tu peut aller dans un channel vocal ? PLUS VITE !");
+            return;
+        }
+
+        var server = server[message.guild.id]= {
+            queue: []
+        };
+
+        server.queue.push(args[1]);
+
+        if(!message.guild.voiceConnection){
+            message.member.voiceChannel.join().then(function(connection){
+                play(connection, message);
+            });
+        }
+
+    }
+
+    if(message.content === 'skip'){
+        var server = servers[message.guil.id];
+        if(server.dispatcher) server.dispatcher.end();
+    }
+
+    if(message.content === stop){
+        var server = servers[message.guil.id];
+        if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+    }
 
 });
 
